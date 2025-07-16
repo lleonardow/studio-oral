4dc0e253801f8b53d09695348d1adc99c2e57ac5
Leonardo Araújo <lleonardoaraujo@gmail.com>
2025-07-16 17:59:15 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 // Lógica para o carrossel de imagens automático
-
 let slideIndex = 0;
 showSlides(); // Inicia o carrossel
 
@@ -21,8 +20,32 @@ function showSlides() {
     }
     
     // Mostra o slide atual
-    slides[slideIndex - 1].style.display = "block";
+    if (slides.length > 0) {
+        slides[slideIndex - 1].style.display = "block";
+    }
     
     // Chama a função novamente a cada 3 segundos (3000 milissegundos)
     setTimeout(showSlides, 3000); 
 }
+
+
+// NOVO: Lógica para esconder o header no scroll (versão mobile)
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener("scroll", function() {
+    // Apenas executa em telas menores que 768px
+    if (window.innerWidth <= 768) {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+        // Garante que o header não se esconda se estivermos no topo da página
+        if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
+            // Scroll para baixo
+            header.classList.add('header-hidden');
+        } else {
+            // Scroll para cima
+            header.classList.remove('header-hidden');
+        }
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Para o caso de scroll no topo
+    }
+}, false);
